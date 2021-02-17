259b73f852ced4e764c4d276705b5ae5f22fbc5d
Jon Zlotnik <me@jonzlotnik.info>
2021-02-17 15:44:50 -0500

    disabled copy constructor

--- EightMinuteEmpireLegends/BiddingFacility/BiddingFacility.h
+++ EightMinuteEmpireLegends/BiddingFacility/BiddingFacility.h
@@ -12,11 +12,13 @@ private:
 	std::unordered_map<std::string, BidSubmission> * bids;
 	std::string * winningPlayerID = nullptr;
 	const BidTieBreaker * tieBreaker;
+
 	std::shared_ptr<std::vector <BidSubmission>> generateBidList();
 	void finalize();
 
 public:
 	BiddingFacility(const BidTieBreaker& tieBreaker);
+	BiddingFacility(const BiddingFacility* biddingFacility) = delete;
 	~BiddingFacility();
 	bool isFinalized();
 	void trySubmitBid(const BidSubmission& bid);
