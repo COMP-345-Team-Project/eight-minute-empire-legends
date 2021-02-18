0bab7cc03ecd57b3fe44e06797b5e7eb1e553e80
Jon Zlotnik <me@jonzlotnik.info>
2021-02-17 19:00:52 -0500

    wrote tests in a driver

--- EightMinuteEmpireLegends/BiddingFacility/BiddingTests.cpp
+++ EightMinuteEmpireLegends/BiddingFacility/BiddingTests.cpp
@@ -9,31 +9,99 @@
 int main(int argc, char** argv) {
 
     BidTieBreakerByLastName tieBreaker;
-    BiddingFacility biddingFacility(tieBreaker);
 
-    BidSubmission bidPlayerA(
-        "plaserA",
-        "playerLastName",
-        22
+
+    /////////////////////////////////////////
+    // Test player with most coins wins
+
+    BiddingFacility t1_biddingFacility(tieBreaker);
+
+    BidSubmission t1_bidPlayerA(
+        "playerA",
+        "aalfredson",
+        0
     );
-    BidSubmission bidPlayerB(
-        "paserB",
-        "klfredson",
-        21
-    );
-    BidSubmission bidPlayerC(
-        "paserB",
+    BidSubmission t1_bidPlayerB(
+        "playerB",
         "balfredson",
-        200
+        3
+    );
+    BidSubmission t1_bidPlayerC(
+        "playerC",
+        "calfredson",
+        2
     );
 
-    biddingFacility.trySubmitBid(bidPlayerA);
-    biddingFacility.trySubmitBid(bidPlayerB);
-    biddingFacility.trySubmitBid(bidPlayerC);
+    t1_biddingFacility.trySubmitBid(t1_bidPlayerA);
+    t1_biddingFacility.trySubmitBid(t1_bidPlayerB);
+    t1_biddingFacility.trySubmitBid(t1_bidPlayerC);
 
-    std::cout <<
-        biddingFacility.getWinningBid().getPlayerLastName()
-        << std::endl;
+    // Assert
+    if (t1_biddingFacility.getWinningBid().getPlayerID() != t1_bidPlayerB.getPlayerID())
+        return 1;
+
+
+    /////////////////////////////////////////
+    // Test player alphabetically first last 
+    // name wins when bids are tied
+
+    BiddingFacility t2_biddingFacility(tieBreaker);
+
+    BidSubmission t2_bidPlayerA(
+        "playerA",
+        "aalfredson",
+        0
+    );
+    BidSubmission t2_bidPlayerB(
+        "playerB",
+        "balfredson",
+        3
+    );
+    BidSubmission t2_bidPlayerC(
+        "playerC",
+        "calfredson",
+        3
+    );
+
+    t2_biddingFacility.trySubmitBid(t2_bidPlayerA);
+    t2_biddingFacility.trySubmitBid(t2_bidPlayerB);
+    t2_biddingFacility.trySubmitBid(t2_bidPlayerC);
+
+    // Assert
+    if (t2_biddingFacility.getWinningBid().getPlayerID() != t2_bidPlayerB.getPlayerID())
+        return 1;
+
+
+
+    /////////////////////////////////////////
+    // Test player alphabetically first last 
+    // name wins when bids are zero
+
+    BiddingFacility t3_biddingFacility(tieBreaker);
+
+    BidSubmission t3_bidPlayerA(
+        "playerA",
+        "aalfredson",
+        0
+    );
+    BidSubmission t3_bidPlayerB(
+        "playerB",
+        "balfredson",
+        0
+    );
+    BidSubmission t3_bidPlayerC(
+        "playerC",
+        "calfredson",
+        0
+    );
+
+    t3_biddingFacility.trySubmitBid(t3_bidPlayerA);
+    t3_biddingFacility.trySubmitBid(t3_bidPlayerB);
+    t3_biddingFacility.trySubmitBid(t3_bidPlayerC);
+
+    // Assert
+    if (t3_biddingFacility.getWinningBid().getPlayerID() != t3_bidPlayerA.getPlayerID())
+        return 1;
 
 
     return 0;
