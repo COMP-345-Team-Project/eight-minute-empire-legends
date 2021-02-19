af40c5c2cdad8f1fc8a93214b89c8d77fe2a5951
goldenfire-yue <35940665+goldenfire-yue@users.noreply.github.com>
2021-02-18 22:03:53 -0500

    No user input required for demo

--- EightMinuteEmpireLegends/Cards/CardsDriver.cpp
+++ EightMinuteEmpireLegends/Cards/CardsDriver.cpp
@@ -13,12 +13,16 @@ int main() {
 
     cout << "This is the cards driver!\n";
 
-    int numPlayer = 0;
+	// Assuming there are 3 players
+    int numPlayer = 3;
+	cout << "Assuming a 3 players game" << endl;
 
+	/*
     while (numPlayer < 2 or numPlayer > 4) {
         cout << "How many players are present? (2-4) \n";
         cin >> numPlayer;
     }
+	*/
 
     //Deck reveal() is included to demo deck functionality, NOT included in actual game
 
@@ -34,6 +38,6 @@ int main() {
         testHand.exchange(testDeck);
         testDeck.reveal();
     }
-    cout << "Hand has been depleted, game ends" << endl;
+    cout << "Hand has been depleted, demo ends" << endl;
 
 }
