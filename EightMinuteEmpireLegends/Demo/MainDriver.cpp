c474f86d04098188566285c48b18980679778d91
Josh Pham <joshpham.concordia@gmail.com>
2021-02-20 13:56:30 -0500

    Added Map Loader to the Main Driver

--- EightMinuteEmpireLegends/Demo/MainDriver.cpp
+++ EightMinuteEmpireLegends/Demo/MainDriver.cpp
@@ -3,6 +3,7 @@
 
 #include "../Map/DriverUtils.h"
 #include "../Map/Map.h"
+#include "../MapLoader/MapLoader.h"
 #include "../BiddingFacility/BiddingFacility.h"
 #include "../BiddingFacility/BidTieBreakerByLastName.h"
 
@@ -137,6 +138,36 @@ int runMapDriver() {
 	return 0;
 }
 
+int runMapLoaderDriver() {
+	MapLoader mapLoader;
+
+	//The file is stored in \eight-minute-empire-legends\EightMinuteEmpireLegends\Debug, make sure you have the map1.json file there
+	std::cout << "Load a valid map file..." << std::endl;
+	Map* map1 = mapLoader.parseMap("../MapLoader/Resources/validJson_validMap_simple.json");
+	delete map1;
+
+	std::cout << "\nLoad a valid map file..." << std::endl;
+	Map* map2 = mapLoader.parseMap("../MapLoader/Resources/validJson_validMap.json");
+	delete map2;
+
+	std::cout << "\nLoad a incorrect file format (CSV)..." << std::endl;
+	Map* map3 = mapLoader.parseMap("../MapLoader/Resources/wrongFormatFile.csv"); //Pointer will be null
+
+	std::cout << "\nLoad a json file, but with missing continents data..." << std::endl;
+	Map* map4 = mapLoader.parseMap("../MapLoader/Resources/invalidJson_missingContinents.json");
+
+	std::cout << "\nLoad a json file, but with missing vertices..." << std::endl;
+	Map* map5 = mapLoader.parseMap("../MapLoader/Resources/invalidJson_missingVertices.json");
+
+	std::cout << "\nLoad a json file, but with wrong data type..." << std::endl;
+	Map* map6 = mapLoader.parseMap("../MapLoader/Resources/invalidJson_wrongDataType.json");
+
+	std::cout << "\nLoad a json file, but it is a disconnected graph..." << std::endl;
+	Map* map7 = mapLoader.parseMap("../MapLoader/Resources/validJson_invalidMap.json");
+
+	return 0;
+}
+
 int runBiddingFacilityDriver() {
     BidTieBreakerByLastName tieBreaker;
 
@@ -239,6 +270,8 @@ int runBiddingFacilityDriver() {
 int main(int argc, char** argv) {
 	std::cout << "(1) Running the Map Driver..." << std::endl;
 	runMapDriver();
+	std::cout << "(2) Running the Map Loader Driver..." << std::endl;
+	runMapLoaderDriver();
 	std::cout << "\n(5) Running the Bidding Facility Driver (the driver uses assertions so there will be no output)..." << std::endl;
     runBiddingFacilityDriver();
 }
