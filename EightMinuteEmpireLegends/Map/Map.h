c6bc01b5404a48dfb2612a181320d3997acc6b28
jaforte <33265690+jaforte@users.noreply.github.com>
2021-02-20 13:24:45 -0500

    A1 map (#13)
    * Created Map and MapDriver cpp and header files.
    
    * Fixed the header file so MapDriver actually builds and executes successfully.
    
    * Finished declarations for all classes in Map.
    
    * Implemented functions for all classes except Map. Implementations subject to change in the future.
    
    * Made further progress into the Map function implementations. Some functions now return pointers instead of object copies.
    
    * Replaced all NULL literals with nullptr. Removed 'using namespace std'.
    
    * Replaced all object references in classes/functions with pointers. Changed logic of Continent::oposite.
    
    * Deleting vertices from graph now properly deletes its incident edges.
    
    * Created null constructors for all classes. Removed inclusion of Lists. Added some useless functions that will no longer exist after the next commit.
    
    * Refactored all classes in Map.cpp and Map.h so subraphs have a more logical structure. All continent members and functions moved to Map class and continents is now a struct.
    
    * Implemented validate() and finalized the map driver.
    
    * Added one extra function in Map, adjacentVertices(), which return a list of vertices adjacent to a given vertex.
    
    * Added starting region as new member for Map class, as well as its respective getter and setter functions.
    
    * Fixed Validate() and cleaned up imports.
    
    * Expanded constraints on insertVertex() so that no two vertices can point to the same territory.
    
    * Destructors added.
    
    * Finalized destructors and moved all function definitions in MapDriver.h to DriverUtils.cpp.

---
+++ EightMinuteEmpireLegends/Map/Map.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// Class forward declarations
+
+class Map;
+struct Continent;
+class Edges;
+class Vertex;
+class Territory;
+
+// Territory class declaration
+
+class Territory
+{
+private:
+	std::string name;
+	std::string owner;	
+	int armies;		
+	std::string const c;
+public:
+	Territory(std::string name, std::string c);
+	Territory(Territory* t);
+	Territory();		
+
+	void setName(std::string name);
+	void setOwner(std::string owner);
+	void setArmies(int armies);
+	std::string getName();
+	std::string getOwner();	
+	int getArmies();
+	std::string getContinent();
+	std::string toString();
+};
+
+// Vertex class declaration
+
+class Vertex
+{
+private:
+	std::string id;
+	Territory* const t;
+public:
+	Vertex(Territory* t, std::string id);
+	Vertex();
+	~Vertex();
+
+	std::string getId();
+	Territory* getTerritory();
+	bool compare(Vertex* v);
+};
+
+// Edge class declaration
+
+class Edge
+{
+private:
+	std::string id;
+	Vertex* v1;
+	Vertex* v2;
+public:
+	Edge(Vertex* v1, Vertex* v2, std::string id);
+	Edge();		
+
+	std::string getId();
+	std::vector<Vertex*> getEndpoints();	
+};
+
+// Continent struct declaration
+
+struct Continent
+{
+	std::string name;
+};
+
+// Map class declaration
+
+class Map
+{
+private:
+	std::string name;
+	std::vector<Vertex*> v_vertices;
+	std::vector<Edge*> v_edges;
+	std::vector<Continent*> v_continents;	
+	Vertex* startingRegion;
+	void cascadeRemoveEdge(std::string id);
+	void dfs(Vertex* v, std::vector<Vertex*>* visited);
+	bool exists(std::string c);
+public:
+	Map(std::string name, std::vector<Continent*> c);
+	Map();
+	~Map();
+	
+	std::string getName();
+	void setStartingRegion(Vertex* v);
+	Vertex* getStartingRegion();
+	int numVertices();
+	std::vector<Vertex*> vertices();		
+	int numEdges();
+	std::vector<Edge*> edges();	
+	int numContinents();
+	std::vector<Continent*> continents();
+	Edge* getEdge(Vertex* v1, Vertex* v2);
+	std::vector<Vertex*> endVertices(Edge* e);
+	Vertex* opposite(Vertex* v, Edge* e);
+	int degree(Vertex* v);
+	std::vector<Edge*> edges(Vertex* v);
+	std::vector<Vertex*> vertices(Continent* c);
+	std::vector<Vertex*> adjacentVertices(Vertex* v);
+	bool insertVertex(Vertex* v);
+	bool insertEdge(Edge* e);
+	bool removeVertex(std::string id);
+	bool removeEdge(std::string id);
+	bool validate();	
+};
