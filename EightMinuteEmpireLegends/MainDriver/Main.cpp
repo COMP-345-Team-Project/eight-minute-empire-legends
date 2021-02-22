a02a694e1d017e292d1acb236d7fc62e5e00f229
iceteaaaa <34047699+iceteaaaa@users.noreply.github.com>
2021-02-21 21:56:01 -0500

    Add Player Class (#25)

--- EightMinuteEmpireLegends/MainDriver/Main.cpp
+++ EightMinuteEmpireLegends/MainDriver/Main.cpp
@@ -16,10 +16,27 @@
 #include "../BiddingFacility/BidSubmission.h"
 #include "../BiddingFacility/BidTieBreakerByLastName.h"
 
+#include "../Player/Player.h"
+
+void runPlayerDriver() {
+    Deck deck(2);
+    BidTieBreakerByLastName bidTieBreakerByLastName;
+    Player* p1 = new Player("player1", deck, bidTieBreakerByLastName);
+
+    p1->BuildCity();
+    p1->DestroyArmy();
+    p1->MoveArmies();
+    p1->MoveOverLand();
+    p1->payCoin();
+    p1->PlaceNewArmies();
+
+    delete p1;
+}
+
 int main() {
     BidTieBreakerByLastName tieBreaker;
 
-
+    runPlayerDriver();
     /////////////////////////////////////////
     // Test player with most coins wins
 
---
+++ EightMinuteEmpireLegends/Player/PlayerDriver.cpp
@@ -0,0 +1,17 @@
+#include "Player.h"
+#include "../BiddingFacility/BidTieBreakerByLastName.h"
+
+int main() {
+	Deck deck(2);
+	BidTieBreakerByLastName bidTieBreakerByLastName;
+	Player* p1 = new Player("player1", deck, bidTieBreakerByLastName);
+
+	p1->BuildCity();
+	p1->DestroyArmy();
+	p1->MoveArmies();
+	p1->MoveOverLand();
+	p1->payCoin();
+	p1->PlaceNewArmies();
+
+	delete p1;
+}
