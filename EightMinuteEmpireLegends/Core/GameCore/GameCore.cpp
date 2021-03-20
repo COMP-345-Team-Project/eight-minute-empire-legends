7cd9147bd0984ae911a88e8fb4d54849538cd40d
jaforte <forte_j@hotmail.com>
2021-03-19 20:55:47 -0400

    Implemented GameBuilder. Added a config file for initial resources.

--- EightMinuteEmpireLegends/Core/GameCore/GameCore.cpp
+++ EightMinuteEmpireLegends/Core/GameCore/GameCore.cpp
@@ -7,28 +7,28 @@ const int _dArmies = 18;
 const int _dCities = 3;
 
 Resources::Resources() {
-    this->coins = 36;   
-    this->armies = 18;  
-    this->cities = 3;   
+	this->coins = 36;
+	this->armies = 18;
+	this->cities = 3;
 }
 
 Resources::Resources(int coins, int armies, int cities) {
-    this->coins = coins;
-    this->armies = armies;
-    this->cities = cities;
+	this->coins = coins;
+	this->armies = armies;
+	this->cities = cities;
 }
 
 void Resources::assignInitialResources(vector<Player*> players) {
-    for (Player* pl : players) {
-        // Assign resources to players
-    }
+	for (Player* pl : players) {
+		// Assign resources to players
+	}
 }
 
 Game::Game(Resources& resources, Map& map, Deck& deck, vector<Player*> players) {
-    this->resources = &resources;
-    this->map = &map;
-    this->deck = &deck;
-    this->players = players;    
+	this->resources = &resources;
+	this->map = &map;
+	this->deck = &deck;
+	this->players = players;
 }
 
 Game* GameBuilder::build(int numPlayers, Map& map) {
