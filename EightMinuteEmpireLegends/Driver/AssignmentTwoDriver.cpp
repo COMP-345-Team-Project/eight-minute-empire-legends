81415ee40da03710d85a94f574c28f9a00ddfc11
jaforte <forte_j@hotmail.com>
2021-03-28 11:24:37 -0400

    Finalized displayTerritories method

--- EightMinuteEmpireLegends/Driver/AssignmentTwoDriver.cpp
+++ EightMinuteEmpireLegends/Driver/AssignmentTwoDriver.cpp
@@ -5,15 +5,15 @@
 int AssignmentTwoDriver::RunGameStartDriver() 
 {
 	GameStart gameStartTest;
-	//gameStartTest.Test_ReadConfigFile_Success();
-	//gameStartTest.Test_ReadConfigFile_Failure();
-	//gameStartTest.Test_BuildGame_Success();
-	//gameStartTest.Test_BuildGame_Failure();
-	//gameStartTest.Test_PlayerInput_SelectPlayerCount_ValidEntry();
-	//gameStartTest.Test_PlayerInput_SelectPlayerCount_InvalidEntry();
-	//gameStartTest.Test_PlayerInput_MapSelect_ValidEntry();
-	//gameStartTest.Test_PlayerInput_MapSelect_InvalidEntry();
-	//gameStartTest.Demo_BuildGame();
+	gameStartTest.Test_ReadConfigFile_Failure();
+	gameStartTest.Test_BuildGame_Success();
+	gameStartTest.Test_BuildGame_Failure();
+	gameStartTest.Test_ReadConfigFile_Success();
+	gameStartTest.Test_PlayerInput_SelectPlayerCount_ValidEntry();
+	gameStartTest.Test_PlayerInput_SelectPlayerCount_InvalidEntry();
+	gameStartTest.Test_PlayerInput_MapSelect_ValidEntry();
+	gameStartTest.Test_PlayerInput_MapSelect_InvalidEntry();
+	gameStartTest.Demo_BuildGame();
 	gameStartTest.Demo_DisplayTerritories();
 	return 0;
 }
