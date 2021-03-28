3122fdb0b8c91b6e7ff8825d797441b5152183fc
jaforte <forte_j@hotmail.com>
2021-03-28 13:07:42 -0400

    Preparing for rebase

--- EightMinuteEmpireLegends/Core/GameCore/GameCore.h
+++ EightMinuteEmpireLegends/Core/GameCore/GameCore.h
@@ -12,10 +12,10 @@ const int _dCoins = 36;
 const int _dArmies = 18;
 const int _dCities = 3;
 
-// ./GameStartTests/Resources
-const std::string _mapDir = "../Tests/GameStartTests/Resources";
-// ./../../config/EightMinuteEmpireLengendsPrefs.ini
-const std::string _configPath = "../../config/EightMinuteEmpireLengendsPrefs.ini";
+// ../Tests/GameStartTests/Resources
+const std::string _mapDir = "D:\\Users\\Forte\\Documents\\GitHub\\eight-minute-empire-legends\\EightMinuteEmpireLegends\\Tests\\GameStartTests\\Resources";
+// ../../config/EightMinuteEmpireLengendsPrefs.ini
+const std::string _configPath = "D:\\Users\\Forte\\Documents\\GitHub\\eight-minute-empire-legends\\config\\EightMinuteEmpireLengendsPrefs.ini";
 
 //This represent the pile of resources provided by the game board
 class Resources {
